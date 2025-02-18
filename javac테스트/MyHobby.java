d30faf9681a1f2a94da76d6adeeb14a5e47b0869
unknown <taelim0619@naver.com>
2025-02-18 14:40:36 +0900

    0218 수업내용

---
+++ javac테스트/MyHobby.java
@@ -0,0 +1,8 @@
+public class MyHobby{
+    public static void main(String [] args){
+    
+    System.out.println("내 취미는"+args[0]+"입니다");
+        System.out.println("내 이름은"+args[1]+"입니다");
+        System.out.println("내 나이는"+args[2]+"세 입니다");
+       }
+}
---
+++ javac테스트/QuizJavac.java
@@ -0,0 +1,12 @@
+public class QuizJavac{
+
+     public static void main(String [] arr){
+     
+     System.out.println("이름 : 최태림");
+     System.out.println("과목 : 자바");
+     System.out.println("메세지 : 안녕!! 열심히 공부해봅시다.");
+
+
+
+     }
+}
